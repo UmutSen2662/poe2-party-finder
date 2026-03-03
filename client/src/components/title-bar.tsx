e9343c69464d13991e167fcd64ffa6243c1278fc
Umut Şen <umutsen2662@gmail.com>
2026-03-03 11:33:54 +0200

    the boxy sidebar+titlebar is done

--- client/src/components/title-bar.tsx
+++ client/src/components/title-bar.tsx
@@ -1,48 +1,58 @@
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import { Minus, Square, X } from "lucide-react";
-import type { ReactNode } from "react";
+import { useSidebar } from "@/components/ui/sidebar";
 
-export function TitleBar({ children }: { children?: ReactNode }) {
+export function TitleBar({ title }: { title: string }) {
   const appWindow = getCurrentWindow();
+  const { state } = useSidebar();
 
   return (
-    <div className="flex flex-col h-screen w-full font-sans antialiased bg-background text-foreground overflow-hidden">
-      <div
-        data-tauri-drag-region
-        className="flex h-9 w-full shrink-0 relative z-50 items-center justify-end select-none bg-sidebar"
-      >
-        {/* Window Controls */}
-        <div className="flex h-full items-center pointer-events-auto overflow-hidden">
-          {/* Minimize Button */}
-          <button
-            type="button"
-            onClick={() => appWindow.minimize()}
-            className="inline-flex h-full w-[46px] flex-shrink-0 items-center justify-center hover:bg-white/10 hover:text-white transition-colors cursor-default"
-          >
-            <Minus strokeWidth={1.5} size={15} />
-          </button>
-
-          {/* Maximize Button */}
-          <button
-            type="button"
-            onClick={() => appWindow.toggleMaximize()}
-            className="inline-flex h-full w-[46px] flex-shrink-0 items-center justify-center hover:bg-white/10 hover:text-white transition-colors cursor-default"
-          >
-            <Square strokeWidth={1.5} size={13} />
-          </button>
-
-          {/* Close Button */}
-          <button
-            type="button"
-            onClick={() => appWindow.close()}
-            className="inline-flex h-full w-[46px] flex-shrink-0 items-center justify-center hover:bg-[#c42b1c] hover:text-white transition-colors cursor-default"
-          >
-            <X strokeWidth={1.5} size={16} />
-          </button>
-        </div>
+    <div
+      data-tauri-drag-region
+      className="flex h-[48px] w-full shrink-0 relative z-50 items-center justify-end select-none bg-sidebar border-b"
+    >
+      {/* App Title */}
+      <div className="mr-auto flex items-center h-full pl-2 overflow-hidden">
+        <span
+          data-tauri-drag-region
+          className={`truncate font-semibold transition-all ease-in-out duration-200 ${
+            state === "collapsed"
+              ? "translate-x-0"
+              : "-translate-x-[10rem] pointer-events-none"
+          }`}
+        >
+          {title}
+        </span>
       </div>
-      <div className="flex-1 w-full overflow-hidden flex flex-col relative">
-        {children}
+
+      {/* Window Controls */}
+      <div className="flex h-full items-center pointer-events-auto overflow-hidden">
+        {/* Minimize Button */}
+        <button
+          type="button"
+          onClick={() => appWindow.minimize()}
+          className="inline-flex h-full w-[46px] flex-shrink-0 items-center justify-center hover:bg-white/10 hover:text-white transition-colors cursor-default"
+        >
+          <Minus strokeWidth={1.5} size={15} />
+        </button>
+
+        {/* Maximize Button */}
+        <button
+          type="button"
+          onClick={() => appWindow.toggleMaximize()}
+          className="inline-flex h-full w-[46px] flex-shrink-0 items-center justify-center hover:bg-white/10 hover:text-white transition-colors cursor-default"
+        >
+          <Square strokeWidth={1.5} size={12} />
+        </button>
+
+        {/* Close Button */}
+        <button
+          type="button"
+          onClick={() => appWindow.close()}
+          className="inline-flex h-full w-[46px] flex-shrink-0 items-center justify-center hover:bg-[#c42b1c] hover:text-white transition-colors cursor-default"
+        >
+          <X strokeWidth={1.5} size={18} />
+        </button>
       </div>
     </div>
   );
--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -5,17 +5,20 @@ import { TitleBar } from "@/components/title-bar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
+const title = "Pact of Exile";
+
 export const Route = createRootRoute({
   component: () => (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <TitleBar>
-        <SidebarProvider className="flex-1 overflow-hidden">
-          <AppSidebar />
-          <ScrollArea className="flex-1 h-full w-full relative">
+      <SidebarProvider className="w-screen h-screen">
+        <AppSidebar title={title} />
+        <div className="flex flex-col flex-1 h-full min-w-0 min-h-0 relative z-0">
+          <TitleBar title={title} />
+          <ScrollArea className="flex-1 min-h-0 w-full relative">
             <Outlet />
           </ScrollArea>
-        </SidebarProvider>
-      </TitleBar>
+        </div>
+      </SidebarProvider>
     </ThemeProvider>
   ),
 });
