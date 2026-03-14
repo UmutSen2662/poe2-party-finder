4b6151a9252f017abb209b0567bc981e3ba93f28
Umut Şen <umutsen2662@gmail.com>
2026-03-14 12:23:44 +0200

    Added relative paths to the website.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,14 +1,20 @@
-// @ts-check
-
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
   },
 });
